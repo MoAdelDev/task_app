7a645d3c8b6490de60d961bee83c992902d322e9
MoAdelDev <m.adeldev10@gmail.com>
2024-06-02 12:27:57 +0300

    - Separate app from main

---
+++ lib/task_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskApp extends StatelessWidget {
+  const TaskApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
