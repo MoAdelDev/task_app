bd628db7a64516ab927c869a2ac8fe7515cd0894
MoAdelDev <m.adeldev10@gmail.com>
2024-06-02 16:56:15 +0300

    - Triggered select filter method

--- lib/features/home/ui/widgets/home_filters.dart
+++ lib/features/home/ui/widgets/home_filters.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:task_app/core/helpers/spacing.dart';
+import 'package:task_app/features/home/logic/cubit/home_cubit.dart';
 import 'package:task_app/features/home/ui/widgets/home_filter_tile.dart';
 
 class HomeFilters extends StatelessWidget {
@@ -8,18 +10,25 @@ class HomeFilters extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 25.h,
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => HomeFilterTile(
-          onTap: () {},
-          isSelected: true,
-          label: 'All',
-        ),
-        separatorBuilder: (context, index) => horizontalSpace(5),
-        itemCount: 4,
-      ),
+    return BlocBuilder<HomeCubit, HomeState>(
+      builder: (context, state) {
+        HomeCubit cubit = context.read();
+        return SizedBox(
+          height: 25.h,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) => HomeFilterTile(
+              onTap: () {
+                cubit.emitSelectFilterState(index);
+              },
+              isSelected: cubit.selectedFilterIndex == index,
+              label: cubit.filters[index],
+            ),
+            separatorBuilder: (context, index) => horizontalSpace(5),
+            itemCount: cubit.filters.length,
+          ),
+        );
+      },
     );
   }
 }
